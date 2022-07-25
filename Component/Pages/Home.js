cc2e859b6768264506295033e3131d2039d0943b
shamrasal <shameshrasal77@gmail.com>
2022-07-25 19:21:11 +0530

    verify email

--- Component/Pages/Home.js
+++ Component/Pages/Home.js
@@ -1,8 +1,47 @@
+import React, { useContext, useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import AuthContext from '../Store/Auth-contex'
 import classes from './home.module.css'
 const Home = () => {
+    const ctx = useContext(AuthContext)
+    const [done, setDone] = useState(false)
+    const verifyHandler = (event) => {
+        event.preventDefault()
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDLwFZKg8KZEIlRPJ_FBc37TP7Vk45D3AE',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    idToken: ctx.token,
+                    requestType: 'VERIFY_EMAIL'
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => {
+            if (res.ok) {
+                res.json().then(data => {
+                    console.log(data)
+                    setDone(true)
+                })
+            } else {
+                res.json().then(data => {
+                    let errorMessage = 'authentication failed...'
+                    if (data && data.error && data.error.message) {
+                        errorMessage = data.error.message
+                    }
+                    alert(errorMessage)
+                })
+            }
+        })
+    }
+
     return (
         <div className={classes.home}>
+            <div className={classes.email}>
+                <button className={classes.verify} onClick={verifyHandler}>Verify Email</button>
+                {done && <p>Verification Email sent</p>}
+            </div>
             <span className={classes.span1}>
                 <div className={classes.name}>
                     <p>"Welcome to Expense Tracker...!!!"</p>
@@ -14,7 +53,7 @@ const Home = () => {
                 </div>
             </span>
             <span className={classes.span2}>
-                
+
             </span>
         </div>
     )
