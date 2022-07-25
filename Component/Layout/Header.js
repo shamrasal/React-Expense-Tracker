26e509f417aee194ab2611c1b96b4b27fc8a7063
shamrasal <shameshrasal77@gmail.com>
2022-07-25 19:36:00 +0530

    user logout button added

--- Component/Layout/Header.js
+++ Component/Layout/Header.js
@@ -1,6 +1,17 @@
+import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import AuthContext from '../Store/Auth-contex'
 import classes from './Header.module.css'
 
 const Header = () => {
+    const history = useHistory()
+    const ctx = useContext(AuthContext)
+    const logoutHandler = (event) => {
+        event.preventDefault()
+        ctx.logOut()
+        history.replace('/signin')
+    }
+
     return (
         <div className={classes.header}>
             <span className={classes.span1}>
@@ -10,6 +21,9 @@ const Header = () => {
                 <h4>Home</h4>
                 <h4>About</h4>
                 <h4>Contact Us</h4>
+            </span>
+            <span className={classes.actions}>
+                <button onClick={logoutHandler}>Log Out</button>
             </span>
         </div>
     )
